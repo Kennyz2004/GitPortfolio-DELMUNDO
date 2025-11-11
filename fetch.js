9fe22b072680b909008fd1434d43c21c79e42fbb
Kennyz2004 <davidkendelmundo@gmail.com>
2025-11-11 10:02:54 +0800

    Update fetch.js

--- fetch.js
+++ fetch.js
@@ -7,6 +7,7 @@ const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 const GITHUB_USERNAME = process.env.GITHUB_USERNAME;
 const USE_GITHUB_DATA = process.env.USE_GITHUB_DATA;
 const MEDIUM_USERNAME = process.env.MEDIUM_USERNAME;
+const USE_MEDIUM_DATA = process.env.USE_MEDIUM_DATA || "false"; 
 
 const ERR = {
   noUserName:
